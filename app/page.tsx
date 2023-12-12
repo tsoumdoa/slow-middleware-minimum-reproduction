46def175d8716062640ad2892573ca5dfd1ccff0
tsoumdoa <tsoumdoa@gmail.com>
2023-12-12 20:02:20 +0900

    semipublic

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,32 @@ import { DownArrow, RightArrow } from "./icons";
 import "./home.css";
 
 export default function Home() {
-  return (
-    <main className="">
-      <article className="grid lg:grid-cols-2">
-        <div className="px-8 py-20 md:px-20 lg:py-48">
-          <h1 className="text-5xl font-semibold text-transparent md:text-6xl gradient">
-            Auth starts here.
-          </h1>
-          <p className="mt-2 text-lg">
-            A simple and powerful Next.js template featuring authentication and
-            user management powered by Clerk.
-          </p>
-          <div className="flex gap-2 mt-8">
-            <Link
-              href="/dashboard"
-              className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
-            >
-              View Demo
-              <div className="m-auto">
-                <RightArrow />
-              </div>
-            </Link>
-            <a
-              className="flex gap-2 px-4 py-2 font-semibold text-gray-600 transition duration-100 rounded-lg hover:text-gray-800"
-              href="#features"
-            >
-              Learn more
-              <div className="m-auto">
-                <DownArrow />
-              </div>
-            </a>
-          </div>
-        </div>
-      </article>
-    </main>
-  );
+	return (
+		<main className="">
+			<article className="grid lg:grid-cols-2">
+				<div className="px-8 py-20 md:px-20 lg:py-48">
+					<h1 className="text-5xl font-semibold text-transparent md:text-6xl gradient">
+						Let me try something weird...
+					</h1>
+					<div className="flex gap-2 mt-8">
+						<Link
+							href="/dashboard"
+							className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
+						>
+							Dashboard
+							<div className="m-auto">
+								<RightArrow />
+							</div>
+						</Link>
+						<Link
+							className="flex gap-2 px-4 py-2 font-semibold text-gray-600 transition duration-100 rounded-lg hover:text-gray-800"
+							href="/semipublic"
+						>
+							Semipublic
+						</Link>
+					</div>
+				</div>
+			</article>
+		</main>
+	);
 }
